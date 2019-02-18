2bc3467b7a7c0b75db33117c88a899610e24996f
J.Andrews U1761083 <U1761083@unimail.hud.ac.uk>
2019-02-18 11:53:15 +0000

    Added Whatever Script

---
+++ GamesJAm/STRANDED/Assets/Scripts/Whatever.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Whatever : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int x = 0; x < 10; x++)
+        {
+            Debug.Log(x);
+        }
+	}
+}
